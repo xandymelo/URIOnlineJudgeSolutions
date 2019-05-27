f4f3004a14969f695a529e1aa52a851a7581f101
Carolina Russi <carolinaferlarussi@gmail.com>
2019-05-26 22:09:04 -0300

    Projeto1001

---
+++ 1001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 0;
+            int B = 0;
+            int X = 0;
+            A = Convert.ToInt32(Console.ReadLine());
+            B = Convert.ToInt32(Console.ReadLine());
+            X = A + B;
+            Console.WriteLine("X = "+X);
+        }
+    }
+}
