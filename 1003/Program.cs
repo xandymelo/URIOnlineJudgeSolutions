210c90665f16f00270560ff562dee729ae22f0f1
Carolina Russi <carolinaferlarussi@gmail.com>
2019-06-01 21:56:32 -0300

    Projeto1003

---
+++ 1003/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            int SOMA;
+            A = Convert.ToInt32(Console.ReadLine());
+            B = Convert.ToInt32(Console.ReadLine());
+            SOMA = A + B;
+            Console.WriteLine($"SOMA = {SOMA}");
+        }
+    }
+}
