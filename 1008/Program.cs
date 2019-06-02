b410a6b7c34d9f08fa17a43985416071d9473b35
Carolina Russi <carolinaferlarussi@gmail.com>
2019-06-01 22:18:01 -0300

    Projeto1008

---
+++ 1008/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int horas;
+            double valorHora;
+            double salario;
+            numero = Convert.ToInt32(Console.ReadLine());
+            horas = Convert.ToInt32(Console.ReadLine());
+            valorHora = Convert.ToDouble(Console.ReadLine());
+            salario = horas * valorHora;
+            Console.WriteLine($"NUMBER = {numero}");
+            Console.WriteLine($"SALARY = U$ {String.Format("{0:0.00}",salario)}");
+        }
+    }
+}
