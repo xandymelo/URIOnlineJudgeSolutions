d9f165c7da218c36bac647f51f79e825e52130fc
Carolina Russi <carolinaferlarussi@gmail.com>
2019-06-01 22:28:39 -0300

    Projeto1011

---
+++ 1011/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radio = Convert.ToDouble(Console.ReadLine());
+            double volume = (4/3.0)*3.14159*(radio*radio*radio);
+            Console.WriteLine($"VOLUME = {volume:f3}");
+            Console.ReadKey();
+        }
+    }
+}
