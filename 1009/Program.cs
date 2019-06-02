8a48115bdc16bf6f62f0252b49446e77b0b621ce
Carolina Russi <carolinaferlarussi@gmail.com>
2019-06-01 22:21:41 -0300

    Projeto1009

---
+++ 1009/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salario;
+            double vendas;
+            double total;
+            nome = (Console.ReadLine());
+            salario = Convert.ToDouble(Console.ReadLine());
+            vendas = Convert.ToDouble(Console.ReadLine());
+            total = ((vendas*0.15) + (salario));
+            total = Math.Round(total, 2);
+            Console.WriteLine($"TOTAL = R$ {total:f2}");
+            Console.ReadKey();
+        }
+    }
+}
