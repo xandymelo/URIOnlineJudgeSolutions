8217e5b48dd7dc34c51c15398e2dd6298b0b87f4
Carolina Russi <carolinaferlarussi@gmail.com>
2019-06-01 22:33:06 -0300

    Projeto1013

---
+++ 1013/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string linha1 = Console.ReadLine();
+
+            String[] values1 = linha1.Split(' ');
+            int a = int.Parse(values1[0]);
+            int b = int.Parse(values1[1]);
+            int c = int.Parse(values1[2]);
+            int MaiorAB = (a+b+Math.Abs(a-b))/2;
+            int MaiorABC = (MaiorAB + c + Math.Abs(MaiorAB - c))/2;
+
+            Console.WriteLine($"{MaiorABC} eh o maior");
+            Console.ReadKey();
+        }
+    }
+}
