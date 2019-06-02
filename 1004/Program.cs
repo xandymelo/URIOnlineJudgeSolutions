72a5386fdce122071b67b7f85c325a386b1f36c9
Carolina Russi <carolinaferlarussi@gmail.com>
2019-06-01 22:01:28 -0300

    Projeto1004

---
+++ 1004/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            int PROD;
+            A = Convert.ToInt32(Console.ReadLine());
+            B = Convert.ToInt32(Console.ReadLine());
+            PROD = A * B;
+            Console.WriteLine($"PROD = {PROD}");
+        }
+    }
+}
