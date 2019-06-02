4bad147caa9c80d18a04933ae49a4c5027d8d02f
Carolina Russi <carolinaferlarussi@gmail.com>
2019-06-01 22:08:04 -0300

    Projeto1005

---
+++ 1005/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A;
+            double B;
+            double media;
+            A = Convert.ToDouble(Console.ReadLine());
+            B = Convert.ToDouble(Console.ReadLine());
+            media = (((A*3.5)) + ((B*7.5)))/(3.5+7.5);
+            Console.WriteLine($"MEDIA = {(string.Format("{0:0.00000}",media))}");
+        }
+    }
+}
