01399e1fb3ed7c07096b658ca2b3c00e036b32d2
Carolina Russi <carolinaferlarussi@gmail.com>
2019-06-01 22:14:46 -0300

    Projeto1007

---
+++ 1007/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            int C;
+            int D;
+            int diferenca;
+            A = Convert.ToInt32(Console.ReadLine());
+            B = Convert.ToInt32(Console.ReadLine());
+            C = Convert.ToInt32(Console.ReadLine());
+            D = Convert.ToInt32(Console.ReadLine());
+            diferenca = (A*B) - (C*D);
+            Console.WriteLine($"DIFERENCA = {diferenca}");
+        }
+    }
+}
