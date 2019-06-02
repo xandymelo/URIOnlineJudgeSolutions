353caaf73f426905efe6b739e5f5e93a8e45efe2
Carolina Russi <carolinaferlarussi@gmail.com>
2019-06-01 22:35:20 -0300

    Projeto1014

---
+++ 1014/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distanciaKm = int.Parse(Console.ReadLine());
+            double litros = Convert.ToDouble(Console.ReadLine());
+            double kmPorLitro = distanciaKm / litros;
+            
+
+            Console.WriteLine($"{kmPorLitro:f3} km/l");
+            Console.ReadKey();
+        }
+    }
+}
