b62cac1a9043ded2a292935f3781ec21a3ce02bb
Carolina Russi <carolinaferlarussi@gmail.com>
2019-06-01 23:16:54 -0300

    Projeto1018

---
+++ 1018/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = int.Parse(Console.ReadLine());
+            Console.WriteLine(valor);
+            int[] valores = new int[] {100, 50, 20, 10, 5, 2, 1}; 
+            int[] cedulas = new int[7];
+            for(int i = 0; i < valores.Length; i++){
+                if ((valor / valores[i]) > 0){ 
+                    cedulas[i] = (valor / valores[i]); 
+                    valor = valor % valores[i]; 
+                }else{ 
+                    cedulas[i] = 0;
+                } 
+            } 
+            for(int i = 0; i < valores.Length; i++){ 
+                Console.WriteLine(String.Format("{0} nota(s) de R$ {1},00", cedulas[i], (double.Parse(valores[i].ToString())))); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
