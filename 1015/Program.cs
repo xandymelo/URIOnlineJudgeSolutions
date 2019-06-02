3349ba931e6e53cd9fbb180ec19c676a1c6ebdc1
Carolina Russi <carolinaferlarussi@gmail.com>
2019-06-01 22:38:32 -0300

    Projeto1015

---
+++ 1015/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string linha1 = Console.ReadLine();
+            string linha2 = Console.ReadLine();
+
+            String[] values1 = linha1.Split(' ');
+            double x1 = Convert.ToDouble(values1[0]);
+            double y1 = Convert.ToDouble(values1[1]);
+
+            String[] values2 = linha2.Split(' ');
+            double x2 = Convert.ToDouble(values2[0]);
+            double y2 = Convert.ToDouble(values2[1]);
+            
+            double distancia = Math.Sqrt((Math.Pow(x2 - x1,2)) + (Math.Pow(y2-y1,2)));
+            Console.WriteLine($"{distancia:f4}");
+            Console.ReadKey();
+        }
+    }
+}
