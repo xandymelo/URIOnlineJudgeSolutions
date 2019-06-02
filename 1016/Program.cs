0de78e383637aa991b4188ac368a56c47bfeccaa
Carolina Russi <carolinaferlarussi@gmail.com>
2019-06-01 22:40:22 -0300

    Projeto1016

---
+++ 1016/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia = int.Parse(Console.ReadLine());
+            int minutos = distancia*2;
+            Console.WriteLine($"{minutos} minutos");
+            Console.ReadKey();
+        }
+    }
+}
