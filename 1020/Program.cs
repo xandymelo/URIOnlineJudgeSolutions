00c113cbf30646404665b87abccafd8720838553
Carolina Russi <carolinaferlarussi@gmail.com>
2019-06-02 21:33:42 -0300

    Projeto1020

---
+++ 1020/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dias = Convert.ToInt32(Console.ReadLine());
+            int ano = dias / 365;
+            dias = dias - (ano * 365);
+            int mes = dias / 30;
+            dias = dias - (mes * 30);
+            Console.WriteLine("{0} ano(s)",ano);
+            Console.WriteLine("{0} mes(es)",mes);
+            Console.WriteLine("{0} dia(s)",dias);
+        }
+    }
+}
