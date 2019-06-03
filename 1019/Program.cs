cdd541b3c73cdd89f3d9ceb49eaa423c9194cbd5
Carolina Russi <carolinaferlarussi@gmail.com>
2019-06-02 21:11:46 -0300

    Projeto1019

---
+++ 1019/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sec = Convert.ToInt32(Console.ReadLine());
+            int h = sec /3600;
+            sec = sec - (h * 3600);
+            int m = sec/60;
+            sec = sec - (m * 60);
+            Console.WriteLine("{0}:{1}:{2}",h,m,sec);
+
+            Console.ReadKey();
+        }
+    }
+}
